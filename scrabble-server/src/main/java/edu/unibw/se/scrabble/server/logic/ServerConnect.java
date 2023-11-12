fdd993b9015da7bc0ff8f728d37db2d55db41776
Jonas Kompalka <j21k0759@unibw.de>
2023-11-12 22:24:45 +0100

    Dokumentation ClientConnect und ServerConnectCallback

--- scrabble-server/src/main/java/edu/unibw/se/scrabble/server/logic/ServerConnect.java
+++ scrabble-server/src/main/java/edu/unibw/se/scrabble/server/logic/ServerConnect.java
@@ -29,6 +29,14 @@ public interface ServerConnect {
         SUCCESSFUL
     }
 
+    /**
+     * Creates a new session for the player if we have less than 10 aktive sessions.
+     * <p>
+     * Never returns {@code null}.
+     *
+     * @param username the user's username
+     * @return {@link ReturnCreateSessionState} enum type, depending on error type or success.
+     */
     ReturnCreateSession createSession(String username);
     record ReturnCreateSession(ReturnCreateSessionState state, int gameID) {}
     enum ReturnCreateSessionState {
@@ -37,6 +45,15 @@ public interface ServerConnect {
         SUCCESSFUL
     }
 
+    /**
+     * Connects the player to the session if the gameID exists and it is enough space.
+     * <p>
+     * Never returns {@code null}.
+     *
+     * @param gameID the ID of the game
+     * @param username the user's username
+     * @return {@link ReturnJoinSession} enum type, depending on error type or success.
+     */
     ReturnJoinSession joinSession(int gameID, String username);
     enum ReturnJoinSession {
         GAME_ID_INVALID,
@@ -44,18 +61,44 @@ public interface ServerConnect {
         SUCCESSFUL
     }
 
+    /**
+     * Starts the game if it has at least 2 player in the session.
+     * <p>
+     * Never returns {@code null}.
+     *
+     * @param username the user's username
+     * @return {@link ReturnStartGame} enum type, depending on error type or success.
+     */
     ReturnStartGame startGame(String username);
     enum ReturnStartGame {
         PLAYER_ALONE_IN_SESSION,
         SUCCESSFUL
     }
 
+    /**
+     * Selects one of the action States.
+     * <p>
+     * Never returns {@code null}.
+     *
+     * @param actionState includes SWAP, PASS and PLACE
+     * @param username the user's username
+     * @return {@link ReturnSelectAction} enum type,  depending on error type or success.
+     */
     ReturnSelectAction selectAction(ActionState actionState, String username);
     enum ReturnSelectAction {
         LESS_THAN_SEVEN_TILES_IN_BAG,
         SUCCESSFUL
     }
 
+    /**
+     * Places a tile of a player if it's a valid position.
+     * <p>
+     * Never returns {@code null}.
+     *
+     * @param tileWithPosition a tile with the exact position on the board (row and column)
+     * @param username the user's username
+     * @return {@link ReturnPlaceTiles} enum type, depending on error type or success.
+     */
     ReturnPlaceTiles placeTiles(TileWithPosition tileWithPosition, String username);
     enum ReturnPlaceTiles {
         TILE_NOT_ON_RACK,
@@ -64,6 +107,14 @@ public interface ServerConnect {
         SUCCESSFUL
     }
 
+    /**
+     * Ends the turn of a player.
+     * <p>
+     * Never returns {@code null}.
+     *
+     * @param username the user's username
+     * @return {@link ReturnEndTurn} enum type successful
+     */
     ReturnEndTurn endTurn(String username);
     enum ReturnEndTurn {
         SUCCESSFUL
